e2b8ad0f251a601c52ca8eefa2adb3e89c721dc1
andrew <andrewshaky50@gamil.com>
2023-01-03 15:33:13 +0200

    counter down added to stats section

--- main.js
+++ main.js
@@ -18,3 +18,37 @@ let counter = setInterval(() => {
     }
 
 }, 1000);
+
+let num= true;
+let countProjects = document.querySelector('.stats .card .projects');
+let countClients = document.querySelector('.stats .card .clients');
+let countCountires = document.querySelector('.stats .card .countries');
+let countmoney = document.querySelector('.stats .card .money');
+projects = 135;
+clients = 150;
+countries = 51;
+money = 500;
+console.log(window.innerHeight)
+let stats = document.querySelector('.stats')
+window.onscroll=()=>{
+        if(num){
+    if(window.scrollY>=stats.offsetTop-250 ){
+        countDown(countProjects,projects,15)
+        countDown(countClients,clients,15)
+        countDown(countCountires,countries,15 )
+        countDown(countmoney,money,0.5)
+    num=false;
+}   
+        }
+    }
+function countDown(obj,limit , time){
+    obj.innerHTML = 0;
+    let handler = setInterval(() => {
+        obj.innerHTML = (+obj.innerHTML) + 1;
+        if(+(obj.innerHTML) > limit){
+            clearInterval(handler);
+        }
+        
+    }, time);
+
+}
